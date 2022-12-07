84cee585dbbe366f8fd8878ddb011e95aeb26ed6
Anton Belov <anton.belov.al7@gmail.com>
2022-12-07 23:07:01 +0000

    added sidebar button

---
+++ components/Navbar.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import Link from "next/link";
+// Install react icons (https://react-icons.github.io/react-icons)
+// import { HiOutlineMenuAlt1 } from 'react-icons/hi';
+// Install react snippets (https://github.com/ults-io/vscode-react-javascript-snippets/blob/HEAD/docs/Snippets.md)
+// rafce
+import React, { useState } from "react";
+import CustomButton from "./CustomButton";
+import Sidebar from "./Sidebar";
+
+const Navbar = () => {
+
+  return (
+    <div className="fixed w-full shadow-xl z-50">
+      <div className="flex justify-between items-center w-full h-full px-5 md:px-10 xl:px-12">
+        <Image
+          src="/../public/skills/nextjs.png"
+          alt="/"
+          width="90"
+          height="60"
+        />
+        <div>
+          <ul className="hidden md:flex">
+            <Link href="/">
+              <li className="ml-10 text-lg uppercase hover:border-b">Home</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-lg uppercase hover:border-b">About</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-lg uppercase hover:border-b">Skills</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-lg uppercase hover:border-b">Projects</li>
+            </Link>
+          </ul>
+        </div>
+        {/* <div className="md:hidden px-3 m-2">
+          <HiOutlineMenuAlt1 size={30} />
+        </div> */}
+        <CustomButton />
+      </div>
+      
+      <Sidebar />
+
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ components/Sidebar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Image from "next/image";
+
+const Sidebar = () => {
+  return (
+    <div className='fixed right-0 top-0 w-full h-screen bg-black/60 -z-10'>
+        <div className='fixed right-0 top-0 w-8/12 sm:w-7/12 md:w-5/12 lg:w-1/3 h-screen bg-slate-50 p-10 ease-in duration-500'>
+            <div>
+                
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Sidebar
